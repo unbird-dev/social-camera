2ca29300ce9a72a28475f5385d155e7d3a276688
Ananthu <ananthusudhikumar@gmail.com>
2020-08-29 17:19:05 +0530

    chore: Added tests for password hashing.

---
+++ server/__tests__/hashing.test.ts
@@ -0,0 +1,17 @@
+import { createHashedPassword, checkHashedPassword } from "src/utility/hashPassword";
+
+describe('Hashing test', () => {
+  const password = 'testPassword';
+  const hashedPassword = createHashedPassword(password);
+  test('Return false -- when password and hashedPassed are not the same', () => {
+    expect(password).not.toStrictEqual(hashedPassword)
+  })
+
+  test('Return true -- when password is correct', () => {
+    expect(checkHashedPassword(password, hashedPassword)).toBe(true);
+  })
+
+  test('Return false -- when password is wrong', () => {
+    expect(checkHashedPassword('wrong', hashedPassword)).toBe(false);
+  })
+})
