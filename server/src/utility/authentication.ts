3765d772ba0fda6d885106ff3c07603695aab64e
Ananthu <ananthusudhikumar@gmail.com>
2020-08-29 19:52:21 +0530

    feat: payload converted to use user id, middleware created.

--- server/src/utility/authentication.ts
+++ server/src/utility/authentication.ts
@@ -1,5 +1,6 @@
 import { genSaltSync, hashSync, compareSync } from 'bcrypt';
 import { sign, verify } from 'jsonwebtoken';
+import { RequestHandler } from 'express';
 
 export const createHashedPassword = (password: string): string => {
   const salt = genSaltSync(parseInt(process.env.SALT_ROUNDS!));
@@ -13,8 +14,8 @@ export const checkHashedPassword = (
   return compareSync(password, hashedPassword);
 };
 
-export const generateAccessToken = (email: string): string => {
-  return sign({ email }, process.env.JWT_TOKEN_SECRET! as string, {
+export const generateAccessToken = (id: string): string => {
+  return sign({ id }, process.env.JWT_TOKEN_SECRET! as string, {
     expiresIn: '1200s'
   });
 };
@@ -22,8 +23,26 @@ export const generateAccessToken = (email: string): string => {
 export const verifyAccessToken = (token: string): string | boolean => {
   try {
     const payload = verify(token, process.env.JWT_TOKEN_SECRET!);
-    return (payload as { email: string }).email;
+    return (payload as { id: string }).id;
   } catch (error) {
     return false;
   }
+};
+
+export const authenticationMiddleware: RequestHandler = (
+  request,
+  response,
+  next
+) => {
+  const header = request.headers['authorization'];
+  const token = header && header.split(' ')[1];
+
+  if (!token) return response.status(401);
+
+  const id = verifyAccessToken(token);
+
+  if (!id) return response.status(403);
+
+  response.locals.id = id;
+  next();
 };
