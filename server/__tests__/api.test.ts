ce4d7171386bad2fc1c62a64e8214bbefc09ae43
Ananthu <ananthusudhikumar@gmail.com>
2020-08-29 23:10:13 +0530

    feat: Upload to presignedUrl.

--- server/__tests__/api.test.ts
+++ server/__tests__/api.test.ts
@@ -1,6 +1,10 @@
 import supertest from 'supertest';
 import app from 'src/server';
 import { generateAccessToken } from 'src/utility/authentication';
+import FormData from 'form-data';
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
+import url from 'url';
 
 const testApp = supertest(app);
 
@@ -9,7 +13,7 @@ describe('S3 apis', () => {
     testApp.get('/get-presigned-url').expect(401, done);
   });
 
-  test('Allow access (200) -- when authorised.', (done) => {
+  test('Allow access (200) -- when authorised.', () => {
     const token = generateAccessToken({
       id: 1,
       email: 'test@gmail.com',
@@ -19,8 +23,29 @@ describe('S3 apis', () => {
     testApp
       .get('/get-presigned-url')
       .set('Authorization', 'Bearer ' + token)
-      .then((response) => {
-        if(response.body) done();
+      .then(({ body }) => {
+        const form = new FormData();
+
+        const { hostname, path, port } = url.parse(body.url);
+
+        form.append(
+          'file',
+          createReadStream(resolve(__dirname, 'assets', '300.png'))
+        );
+
+        form.submit(
+          {
+            hostname,
+            port,
+            path,
+            method: "PUT"
+          },
+          (error, response) => {
+            response.resume();
+
+            expect(response.statusCode).toBe(200)
+          }
+        );
       });
   });
 });
